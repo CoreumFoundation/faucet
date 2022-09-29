1521040be4fd8e5b1b4f956534a2cffaeae2b909
Milad Zahedi <mza1989@gmail.com>
2022-09-29 15:06:14 +0330

    addressed PR comments

--- http/gen_funded.go
+++ http/gen_funded.go
@@ -2,20 +2,20 @@ package http
 
 import "github.com/CoreumFoundation/faucet/pkg/http"
 
-// SendMoneyGenPrivkeyResponse is the output to GiveFunds request
-type SendMoneyGenPrivkeyResponse struct {
+// GenFundedResponse is the output to GiveFunds request
+type GenFundedResponse struct {
 	TxHash   string `json:"txHash"`
 	Mnemonic string `json:"mnemonic"`
 	Address  string `json:"address"`
 }
 
-func (h HTTP) sendMoneyGenPrivkeyHandle(ctx http.Context) error {
+func (h HTTP) genFundedHandle(ctx http.Context) error {
 	rsp, err := h.app.GenPrivkeyAndFund(ctx.Request().Context())
 	if err != nil {
 		return err
 	}
 
-	return ctx.JSON(200, SendMoneyGenPrivkeyResponse{
+	return ctx.JSON(200, GenFundedResponse{
 		TxHash:   rsp.TxHash,
 		Mnemonic: rsp.Mnemonic,
 		Address:  rsp.Address,
