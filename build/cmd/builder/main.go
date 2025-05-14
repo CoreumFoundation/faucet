0a5c17aec834e04a1eefcf299bc8f52a5d5cef86
Masih Yeganeh <goodboy.php@gmail.com>
2025-05-14 14:15:40 +0330

    Use coreum tools

--- build/cmd/builder/main.go
+++ build/cmd/builder/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	coreumTools "github.com/CoreumFoundation/coreum/build/tools"
 	"github.com/CoreumFoundation/crust/build"
 	"github.com/CoreumFoundation/crust/build/tools"
 	selfBuild "github.com/CoreumFoundation/faucet/build"
-	selfTools "github.com/CoreumFoundation/faucet/build/tools"
 )
 
 func init() {
-	tools.AddTools(selfTools.Tools...)
+	tools.AddTools(coreumTools.Tools...)
 }
 
 func main() {
