47126ebf95d625ea11e10de8f498ea2096b0969a
Masih Yeganeh <goodboy.php@gmail.com>
2025-05-14 13:43:39 +0330

    Add tools

--- build/cmd/builder/main.go
+++ build/cmd/builder/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/CoreumFoundation/crust/build"
+	"github.com/CoreumFoundation/crust/build/tools"
 	selfBuild "github.com/CoreumFoundation/faucet/build"
+	selfTools "github.com/CoreumFoundation/faucet/build/tools"
 )
+
+func init() {
+	tools.AddTools(selfTools.Tools...)
+}
 
 func main() {
 	build.Main(selfBuild.Commands)
